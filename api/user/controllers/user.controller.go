077755666609b1af58664fd2fcc3d82ff3e29a00
joao m lucio <joaomatheuslimalucio@gmail.com>
2023-01-17 07:11:55 -0300

    [#] modifying project structure

---
+++ api/user/controllers/user.controller.go
@@ -0,0 +1,16 @@
+package controllers
+
+func GetAll(c *fiber.Ctx) error {
+	users, err := findUsers()
+	if err != nil {
+		return c.Status(404).SendString(err.Error())
+	}
+	
+	blob, err := json.Marshal(users)
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+	
+	c.Response().BodyWriter().Write(blob)
+	return c.SendStatus(200)
+}
